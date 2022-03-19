b6d1128ecb09e559815b35a3c0da285779185098
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-03-19 14:48:48 -0300

    fix: removing exception implementation from InvalidFormatBoletoException

--- lib/exceptions.dart
+++ lib/exceptions.dart
@@ -2,7 +2,7 @@ import 'package:boleto/boleto.dart';
 
 abstract class BoletoException implements Exception {}
 
-class InvalidFormatBoletoException extends Boleto implements Exception {
+class InvalidFormatBoletoException extends Boleto {
   @override
   String toString() {
     return "Given line should contain 47 or 48 characters";
